95a5f93eda5474250998d74ddc2c0b7d46e5d775
mteo <cortinamateo211@gmail.com>
2026-08-14 17:06:18 -0500

    refactor: delete nixvim

--- flake.nix
+++ flake.nix
@@ -1,13 +1,8 @@
 {
-  description = "Configuración NixOS de s";
+  description = "Configuración NixOS de mteo";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    nixvim = {
-      url = "github:nix-community/nixvim";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
 
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -15,18 +10,15 @@
     };
   };
 
-  outputs = { self, nixpkgs, nixvim, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations = {
-      # Cambia "nixos" por el hostname exacto de tu PC si no se llama "nixos"
+      
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = { inherit inputs; };
         modules = [
           ./hardware-configuration.nix
           ./configuration.nix
-          
-          nixvim.nixosModules.nixvim
-	  ./home-modules/nixvim.nix
 
           home-manager.nixosModules.home-manager
           {
