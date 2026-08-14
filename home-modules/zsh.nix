f9db16275138183a71bf29c2650548a7c5b23ed4
mteo <cortinamateo211@gmail.com>
2026-08-14 09:49:30 -0500

    feat: zsh

--- home-modules/zsh.nix
+++ home-modules/zsh.nix
@@ -46,15 +46,15 @@
       cc = "clear && pokemon-colorscripts -r --no-title";
       y = "yazi";
       gg = "lazygit";
-      nir = "cd ~/.config/niri/";
-      nz = "nvim /etc/nixos/home-modules/zsh.nix"; # Actualizado a tu nuevo archivo en NixOS
+      ni = "cd /etc/nixos";
+      nz = "sudo nvim /etc/nixos/home-modules/zsh.nix"; 
       n = "nvim";
       pk = "pokemon-colorscripts -r --no-title";
       pkk = "pokemon-colorscripts -n";
-      update = "sudo nixos-rebuild switch"; # Actualizado para NixOS (reemplaza a dnf)
+      nis = "sudo nixos-rebuild switch"; 
+      nit = "sudo nixos-rebuild test"; 
     };
 
-    # Comandos que se ejecutan al abrir la terminal (como tu script de Pokémon)
     initExtra = ''
       pokemon-colorscripts -r --no-title
     '';
