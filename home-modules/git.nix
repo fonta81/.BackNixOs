7607b7dc8633eada29e2cb7680756606a110c6bb
mteo <cortinamateo211@gmail.com>
2026-08-17 17:28:12 -0500

    docs(git): --comentarios

--- home-modules/git.nix
+++ home-modules/git.nix
@@ -1,14 +1,5 @@
 {config,pkgs,...}:
 {
-  # programs.git = {
-  #   enable = true;
-  #   userName = "mteo";
-  #   userEmail = "cortinamateo211@gmail.com";
-  #
-  #   extraConfig = {
-  #     safe.directory = "/etc/nixos";
-  #   };
-  # };
     programs.git = {
       enable = true;
       settings = {
