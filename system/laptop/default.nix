1a7b4e92f23d064d6cb66810ebd0afb6073e2379
mteo <cortinamateo211@gmail.com>
2026-08-21 19:51:59 -0500

    feat(Laptop): Bluetooth

--- system/laptop/default.nix
+++ system/laptop/default.nix
@@ -1,8 +1,12 @@
 { pkgs, ... }: {
+
   networking.hostName = "laptop";
 
-  # Cosas exclusivas de la laptop
-  # services.tlp.enable = true; # Ahorro de batería
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
+
   # environment.systemPackages = with pkgs; [
   #   brightnessctl # Control de brillo de pantalla
   # ];
