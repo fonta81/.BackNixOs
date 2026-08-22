220dee8ac6d9b21593da42e14be89626e5acd31a
mteo <cortinamateo211@gmail.com>
2026-08-21 20:04:33 -0500

    feat(Laptop): Config Bluetooth

--- system/laptop/default.nix
+++ system/laptop/default.nix
@@ -2,9 +2,17 @@
 
   networking.hostName = "laptop";
 
+
+  services.dbus.enable = true;
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
+    settings = {
+        General = {
+          Enable = "Source,Sink,Media,Socket"; # Asegura compatibilidad con audio
+          Experimental = true; # Habilita funciones avanzadas (como ver batería de dispositivos)
+        };
+    };
   };
 
   # environment.systemPackages = with pkgs; [
