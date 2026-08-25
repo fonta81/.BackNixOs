7286c3be1a181785120b3146288c98c03731f964
mteo <cortinamateo211@gmail.com>
2026-08-25 18:33:17 -0500

    refactor(laptop): experimental = false

--- system/hosts/laptop/default.nix
+++ system/hosts/laptop/default.nix
@@ -10,7 +10,7 @@
     settings = {
         General = {
           Enable = "Source,Sink,Media,Socket"; 
-          Experimental = true; 
+          # Experimental = true; 
         };
     };
   };
