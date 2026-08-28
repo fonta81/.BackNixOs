b62640de4cfc7391463d9b1add60bee927375381
mteo <cortinamateo211@gmail.com>
2026-08-28 07:56:20 -0500

    refactor(config): test delete gdm

--- configuration.nix
+++ configuration.nix
@@ -39,7 +39,7 @@
 
   # Enable the GNOME Desktop Environment.
   services.desktopManager.gnome.enable = true;
-  services.displayManager.gdm.enable = true;
+  # services.displayManager.gdm.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
