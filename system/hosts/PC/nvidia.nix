eede822b1e1c61c0179288dbe4a3d51250a9ad41
mteo <cortinamateo211@gmail.com>
2026-09-04 07:30:01 -0500

    Docs(nvidia.nix): delete coments

--- system/hosts/PC/nvidia.nix
+++ system/hosts/PC/nvidia.nix
@@ -1,25 +1,20 @@
 { config, lib, pkgs, ... }:
 {
-  # Necesario para renderizado gráfico moderno (Vulkan/OpenGL) en Wayland
   hardware.graphics.enable = true;
 
   services.xserver.videoDrivers = [ "nvidia" ];
 
   hardware.nvidia = {
-    # Obligatorio para Wayland: habilita KMS (kernel modesetting)
+
     modesetting.enable = true;
 
-    # OBLIGATORIO para RTX 50 (Blackwell): el módulo propietario clásico
-    # ya no es compatible con esta arquitectura.
     open = true;
 
-    # Recomendado dejar en false salvo que tengas problemas de suspensión
     powerManagement.enable = false;
     powerManagement.finegrained = false;
 
     nvidiaSettings = true;
 
-    # Usa el driver "stable" más reciente disponible en tu canal de nixpkgs
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 }
