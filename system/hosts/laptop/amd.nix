14088bb0bf2f6c43a9d5835baf0c05e395a94049
mteo <cortinamateo211@gmail.com>
2026-09-04 07:42:01 -0500

    refactor(amd.nix): extra pkgs

--- system/hosts/laptop/amd.nix
+++ system/hosts/laptop/amd.nix
@@ -1,14 +1,18 @@
+
 { config, lib, pkgs, ... }:
 
 {
-
   boot.initrd.kernelModules = [ "amdgpu" ];
+  
+  services.xserver.videoDrivers = [ "amdgpu" ];
 
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
+    extraPackages = with pkgs; [
+      vaapiVdpau
+      libvdpau-va-gl
+      rocmPackages.clr.icd
+    ];
   };
-
-  services.xserver.videoDrivers = [ "amdgpu" ];
-
 }
