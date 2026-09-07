4c01a9b9851dd63663bdbb8b1cd92d2d52d49255
mteo <cortinamateo211@gmail.com>
2026-09-07 08:29:49 -0500

    feat(nvidia.nix): 32bits

--- system/hosts/PC/nvidia.nix
+++ system/hosts/PC/nvidia.nix
@@ -1,20 +1,18 @@
 { config, lib, pkgs, ... }:
 {
-  hardware.graphics.enable = true;
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
 
   services.xserver.videoDrivers = [ "nvidia" ];
 
   hardware.nvidia = {
-
     modesetting.enable = true;
-
     open = true;
-
     powerManagement.enable = false;
     powerManagement.finegrained = false;
-
     nvidiaSettings = true;
-
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 }
