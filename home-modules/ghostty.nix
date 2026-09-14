7c7990cdc3eb89da2ff5d597af7e48283034dbe3
mteo <cortinamateo211@gmail.com>
2026-09-14 07:17:01 -0500

    style(ghostty): space

--- home-modules/ghostty.nix
+++ home-modules/ghostty.nix
@@ -3,13 +3,13 @@
 {
   programs.ghostty = {
     enable = true;
-    
+
     # Opciones de Ghostty
     settings = {
       theme = "dankcolors";
       font-family = "JetBrainsMono Nerd Font";
       font-size = 11;
-      
+
       # Forzar a Ghostty a usar Zsh (en Linux por defecto toma la $SHELL del usuario)
       command = "${pkgs.zsh}/bin/zsh";
     };
